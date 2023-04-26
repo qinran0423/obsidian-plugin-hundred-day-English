1a31e28fd7c3472ea069dc58a24549098d6a3187
qinran0423 <1156107187@qq.com>
2023-04-26 22:17:34 +0800

    fix: ts build js

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from '@rollup/plugin-commonjs';
-
+import typescript from '@rollup/plugin-typescript';
 
 export default {
 	input: 'src/main.ts',
@@ -9,6 +9,7 @@ export default {
 		format: 'cjs'
 	},
 	plugins: [
+		typescript(),
 		nodeResolve(),
 		commonjs({
 			extensions:['.js','.json']
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,3 @@
 
 let a: string = '1'
+console.log(a)
